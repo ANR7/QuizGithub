1283cfaddfae935af7b4776d6271bc092eb707bb
ANR <r.anouar7@gmail.com>
2018-03-08 01:50:37 +0100

    Rajout d'une fonction dans la fonction

--- fonction.swift
+++ fonction.swift
@@ -1,9 +1,14 @@
-// On déclare la fonction
+// On déclare les fonctions
 func disBonjour() {
     print("Bonjour !")
 }
 
+func disBonjourCommentCaVa() {
+    disBonjour()
+    print("Comment ça va ?")
+}
+
 // Enfin, on peut s'en servir !
-disBonjour()
+disBonjourCommentCaVa()
 
 
